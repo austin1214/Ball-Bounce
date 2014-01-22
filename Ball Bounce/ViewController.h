eee561cc97d8ac526c5045d3f03b2c5de812d204
Austin Brogdon <swaggychicken@gmail.com>
2014-01-21 21:57:49 -0500

    Initial Commit

---
+++ Ball Bounce/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ball Bounce
+//
+//  Created by HDGamer1214 on 1/21/14.
+//  Copyright (c) 2014 Austin Brogdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ball BounceTests/Ball_BounceTests.m
@@ -0,0 +1,34 @@
+//
+//  Ball_BounceTests.m
+//  Ball BounceTests
+//
+//  Created by HDGamer1214 on 1/21/14.
+//  Copyright (c) 2014 Austin Brogdon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ball_BounceTests : XCTestCase
+
+@end
+
+@implementation Ball_BounceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
